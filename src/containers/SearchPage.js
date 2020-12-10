4d9b033f7666a12fdae4487398cf0c41351fb3d2
Hayato Sugimoto <10haya22ton@gmail.com>
2020-12-10 22:31:25 +0900

    検索ボタン入りのテキストボックスをコンポーネント化

--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { PrimaryButton, TextBox} from '../components/UIkit';
+import { PrimaryButton, TextBox, SearchInputBase} from '../components/UIkit';
+import { createStyles } from '@material-ui/core';
+import {makeStyles} from "@material-ui/styles";
 
-const SearchPage = () => {
+const useStyles = makeStyles(() => {
+    createStyles({
+        'root' : {
+            padding: '2px 4px',
+            display: 'flex',
+            alighnItems: 'center',
+            width: 400
+        },
+        'input': {
+            flex: 1
+        }
+    })
+});
+
+
+const SearchPage = (props) => {
+    const classes = useStyles();
+
     return(
         <div>
             チャレンジや本のタイトルを入力してください
-            <TextBox label={"チャレンジ名、本のタイトル、カテゴリ"} />
-            <PrimaryButton label={"検索"} />
+            <SearchInputBase placeholder={'検索'} />
         </div>
     );
 }
