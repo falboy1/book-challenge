7cef4db63153f832f71a01f153366b95462ec7ec
Hayato Sugimoto <10haya22ton@gmail.com>
2020-12-14 01:56:59 +0900

    firebase authで会員登録の処理を追加

--- src/containers/SignUp.js
+++ src/containers/SignUp.js
@@ -1,43 +1,88 @@
-import React from 'react';
-import { makeStyles} from "@material-ui/core/styles";
+import React , {useCallback, useState} from 'react';
 import {TextBox, PrimaryButton} from '../components/UIkit';
-
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-        width: '25ch',
-      },
-    },
-  }));
+import {signUp} from '../reducks/users/operation';
+import {useDispatch} from 'react-redux';
 
 
 const SignUp = () => {
-    const classes = useStyles();
-    return (
+    const dispatch = useDispatch()
+
+  // Hook 
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    // callback
+    const inputUsername = useCallback((event)=> {
+        setUsername(event.target.value)
+    }, [setUsername]);
+
+    const inputEmail = useCallback((event)=> {
+        setEmail(event.target.value)
+    }, [setEmail]);
+
+    const inputPassword = useCallback((event)=> {
+        setPassword(event.target.value)
+    }, [setPassword]);
+
+    const inputConfirmPassword = useCallback((event)=> {
+        setConfirmPassword(event.target.value)
+    }, [setConfirmPassword]);
+  
+
+  return (
         <div className={'center'}>
             <div className={'vertical-space-40'} />
             <h2>新規会員登録</h2>
             <form noValidate autoComplete="off">
                 <TextBox 
-                  label={"メールアドレス"} 
-                  width={'300px'}/>
+                  label={"メールアドレス"}
+                  value={email}
+                  required={true}
+                  onChange={inputEmail}
+                  type={'email'}
+                  width={'300px'}
+                />
                 <TextBox 
-                  label={"ID"}
+                  label={"ユーザ名"}
+                  value={username}
+                  required={true}
                   placeholder={'半角英数字6文字以上'}
-                  width={'300px'}/>
+                  onChange={inputUsername}
+                  type={'text'}
+                  width={'300px'}
+                />
                 <TextBox
                   label={"パスワード"}
+                  value={password}
+                  required={true}
                   placeholder={'半角英数字6文字以上'}
-                  width={'300px'}/>
+                  onChange={inputPassword}
+                  type={'password'}
+                  width={'300px'}
+                />
+                <TextBox
+                  label={"パスワード（再確認）"}
+                  value={confirmPassword}
+                  required={true}
+                  placeholder={'半角英数字6文字以上'}
+                  onChange={inputConfirmPassword}
+                  type={'password'}
+                  width={'300px'}
+                />
                 <div className={'vertical-space-20'}/>
                 <div>
-                  <PrimaryButton label={"登録する"} width={'300px'} padding={'15px 10px'}/>
+                  <PrimaryButton 
+                    label={"登録する"}
+                    width={'300px'} 
+                    padding={'15px 10px'}
+                    onClick={() => dispatch(signUp(username, email, password, confirmPassword))}
+                  />
                 </div>
             </form>
         </div>
-    );
+  );
 }
 
 export default SignUp;
