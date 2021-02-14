9515723bd2b93253e0ad16ab1dc15afbcde45f97
Hayato Sugimoto <10haya22ton@gmail.com>
2021-02-14 17:49:31 +0900

    アコーディオン修正

---
+++ src/components/UIkit/Accordion.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
+    },
+}));
+
+const SimpleAccordion = (props) => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    {props.summary}
+                </AccordionSummary>
+
+                <AccordionDetails>
+                    {props.details}
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
+}
+
+export default SimpleAccordion;
